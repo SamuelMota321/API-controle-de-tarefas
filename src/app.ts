abbe2637552b7af59bca0caf130bd560d1f343f4
Samuel Rodrigues da Mota <138218205+SamuelMota321@users.noreply.github.com>
2024-08-24 20:16:51 -0300

    Update for deploy

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import "dotenv/config"
 import "express-async-errors";
 import express, { json } from "express";
 import helmet from "helmet";
+import cors from "cors";
 import { taskRouter } from "./routers/task.routes";
 import { HandleErrors } from "./middlewares/handleError.middleware";
 import { categoriesRouter } from "./routers/categories.routes";
@@ -12,6 +13,7 @@ export const app = express();
 
 app.use(helmet());
 app.use(json());
+app.use(cors())
 app.use("/tasks", taskRouter);
 app.use("/categories", categoriesRouter);
 app.use("/users", userRouter);
